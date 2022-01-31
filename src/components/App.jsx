96455f1111d5b24f712cfb1310b7a079758d13d4
mt-andrea <mateandicica@gmail.com>
2022-01-31 18:56:10 +0100

    start

---
+++ src/components/App.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import Szamlalo from "./Szamlalo";
+
+function App() {
+  return <Szamlalo />;
+}
+
+export default App;
---
+++ src/components/Szamlalo.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Szamlalo() {
+  return (
+    <div className="container">
+      <h1>0</h1>
+      <button>-</button>
+      <button>+</button>
+    </div>
+  );
+}
+
+export default Szamlalo;
