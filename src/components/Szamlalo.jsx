87ee7587e2ce4399fa30995e319982349607de23
mt-andrea <mateandicica@gmail.com>
2022-01-31 19:08:09 +0100

    dec inc zero

--- src/components/Szamlalo.jsx
+++ src/components/Szamlalo.jsx
@@ -1,11 +1,27 @@
-import React from "react";
+import React, {useState} from "react";
 
 function Szamlalo() {
+
+  const [count, setCount]=useState(0)
+
+  function novel() {
+    setCount(count+1)
+  }
+
+function csokkent() {
+  setCount(count-1)
+}
+
+function zero() {
+  setCount(0)
+}
+
   return (
     <div className="container">
-      <h1>0</h1>
-      <button>-</button>
-      <button>+</button>
+      <h1 style={count>10 ? {color:"red"} : {color: "white"}}>{count}</h1>
+      <button onClick={csokkent}>-</button>
+      <button onClick={novel}>+</button>
+      <button onClick={zero}>0</button>
     </div>
   );
 }
